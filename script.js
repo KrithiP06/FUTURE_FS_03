96c2270a133d56fb134f25fec5fcd5518429e4ef
Krithi Prakash <19736krithi@gmail.com>
2026-05-06 20:13:00 +0530

    Refactor script.js to remove form handling
    Removed contact form submission logic and added scrollToMenu function.

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
-document.getElementById("contactForm").addEventListener("submit", async (e) => {
-e.preventDefault();
-
-const data = {
-name: e.target[0].value,
-email: e.target[1].value,
-message: e.target[2].value
-};
-
-try {
-const res = await fetch("http://127.0.0.1:5000/contact", {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
-},
-body: JSON.stringify(data)
-});
-
-```
-if (res.ok) {
-  alert("Message sent successfully!");
-} else {
-  alert("Something went wrong");
+javascript
+function scrollToMenu() {
+    document.getElementById("menu").scrollIntoView({
+        behavior: "smooth"
+    });
 }
-```
-
-} catch (err) {
-alert("Server not running");
-}
-});
